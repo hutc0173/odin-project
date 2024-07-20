0c59d0eb4b969d20e479083974d701747448bfe3
Sammy Hutchinson <hutc0173@algonquinlive.com>
2024-07-20 17:04:35 -0400

    rock paper scissors game

---
+++ rock-paper-scissors/script.js
@@ -0,0 +1,55 @@
+console.log("hello world");
+
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3);
+  if (randomNumber === 0) {
+    return "Rock";
+  } else if (randomNumber === 1) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
+}
+
+function getHumanChoice(callback) {
+  const readline = require("readline");
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.question("Please enter a 'Rock, Paper or Scissors: ", (answer) => {
+    const humanChoice = answer.trim().toLowerCase();
+    console.log(`You chose: ${humanChoice}`);
+    rl.close();
+    callback(humanChoice);
+  });
+}
+
+function playRound(humanChoice, computerChoice) {
+  console.log(`Computer chose: ${computerChoice}`);
+  if (computerChoice.toLowerCase() === humanChoice) {
+    console.log("Its a draw");
+  } else if (
+    (computerChoice === "Rock" && humanChoice === "Scissors") ||
+    (computerChoice === "Scissors" && humanChoice === "Paper") ||
+    (computerChoice === "Rock" && humanChoice === "Paper")
+  ) {
+    console.log(`Computer wins! ${computerChoice} beats ${humanChoice}`);
+    computerScore++;
+  } else {
+    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    humanScore++;
+  }
+
+  console.log(`Scores - Human: ${humanScore}, Computer: ${computerScore}`);
+}
+
+getHumanChoice((humanChoice) => {
+  const computerChoice = getComputerChoice();
+  playRound(humanChoice, computerChoice);
+});
