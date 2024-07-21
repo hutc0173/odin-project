953576c2c4bd173da29273df2a3af9b9d7eb5c58
Sammy Hutchinson <hutc0173@algonquinlive.com>
2024-07-21 09:33:00 -0400

    Removed embedded repositories from index

--- fundamentals/fundamentals.js
+++ fundamentals/fundamentals.js
@@ -1,28 +1,28 @@
-const readline = require("readline");
+// const readline = require("readline");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
 
-rl.question("Please enter a number: ", (answer) => {
-  const num = parseInt(answer);
+// rl.question("Please enter a number: ", (answer) => {
+//   const num = parseInt(answer);
 
-  if (!isNaN(num)) {
-    for (let i = 1; i <= num; i++) {
-      if (i % 3 === 0 && i % 5 === 0) {
-        console.log("FizzBuzz");
-      } else if (i % 5 === 0) {
-        console.log("Buzz");
-      } else if (i % 3 === 0) {
-        console.log("Fizz");
-      } else {
-        console.log(i);
-      }
-    }
-  } else {
-    console.log("That is not a valid number.");
-  }
+//   if (!isNaN(num)) {
+//     for (let i = 1; i <= num; i++) {
+//       if (i % 3 === 0 && i % 5 === 0) {
+//         console.log("FizzBuzz");
+//       } else if (i % 5 === 0) {
+//         console.log("Buzz");
+//       } else if (i % 3 === 0) {
+//         console.log("Fizz");
+//       } else {
+//         console.log(i);
+//       }
+//     }
+//   } else {
+//     console.log("That is not a valid number.");
+//   }
 
-  rl.close();
-});
+//   rl.close();
+// });
